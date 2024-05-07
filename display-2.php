1adcebe40eef57fe306aa6b8cdfa2181102e4c4f
RIA-DHIMAN <riadhiman3729@gmail.com>
2024-05-07 13:49:16 +0530

    add files

--- display-2.php
+++ display-2.php
@@ -24,6 +24,7 @@ include "preloder.php";
       <button type="submit" class="button" name="pin">Submit</button>
     </form>
   </div>
+  <button class="button-exit"><a href="logout.php"class="bt-text">EXIT</a></button>
     </section>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- querry.php
+++ querry.php
@@ -1,18 +1,22 @@
 <?php
 include "script.php";
 if(isset($_POST['pin'])){
-     echo $account_no =$_POST['account_no'];
+    $account_no =$_POST['account_no'];
    
     $select="select * from information where account_no = '$account_no'";
     $querry=mysqli_query($con,$select);
-    if($querry){
+    $row= $querry -> fetch_assoc();
+    if($row>0){
         while ($ria = $querry->fetch_assoc()) {
             echo $sid = $ria['account_no'];
         }
-        session_start();
          $_SESSION['sid'] = $account_no;
+         echo $_SESSION['sid'];
         header("Location: display-3.php");
+    }else{
+        header("Location: notfound.php");
     }
+    
 }
 if(isset($_POST['balance'])){
     // $account_no =$_POST['accountn_no'];

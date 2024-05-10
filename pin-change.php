833192b781ae800caab76016b0490f4bae29ded9
RIA-DHIMAN <riadhiman3729@gmail.com>
2024-05-10 22:19:30 +0530

    add files

--- pin-change.php
+++ pin-change.php
@@ -17,32 +17,22 @@ include "preloder.php";
     <hr><br>
     <div class="pin-form">
     <h3>PLEASE ENTER YOUR PHONE NUMBER</h3>
-    <form>
+    <form method="post" action="script-2.php">
       <input type="number" id="phone" name="phon_eno" placeholder="Enter yous phone number">
-    </form>
-    <br>
-    <h3>PLEASE ENTER YOUR OLD PIN</h3>
-    <br>
-    <form>
-      <input type="password" id="pin" name="pin" placeholder="New PIN">
-     
-    </form>
-    <h3>PLEASE ENTER YOUR PIN</h3>
-    <br>
-    <form>
-      <input type="password" id="pin" name="pin" placeholder="New PIN">
-      
-    </form>
-    <h3>CONFIRM PIN</h3>
-    <form>
-    <input type="password" id="pin" name="pin" placeholder="Confirm PIN">
-      <button type="submit" class="button">Submit</button>
-    </form>
+      <h3>PLEASE ENTER YOUR OLD PIN</h3>
+      <input type="password" id="pin" name="old-pin" placeholder="New PIN">
+      <h3>PLEASE ENTER YOUR PIN</h3>
+      <input type="password" id="pin" name="new-pin" placeholder="New PIN">
+      <h3>CONFIRM PIN</h3>
+      <input type="password" id="pin" name="c-pin" placeholder="Confirm PIN">
+      <br>
+      <button type="submit" class="button" name="new-submit">Submit</button>
+  </form>
   </div>
    </div>
    <br>
    <button class="button-exit"><a href="logout.php"class="bt-text">EXIT</a></button>
-</section>
+  </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
--- script-2.php
+++ script-2.php
@@ -14,4 +14,26 @@ if(isset($_POST['cash'])){
     $updatequerry= mysqli_query($con,$updatesql);
     header("Location:amt-withdrawn.php");
 }
+if(isset($_POST['new-submit'])){
+    $old_pin=$_POST['old-pin'];
+    $new_pin=$_POST['new-pin'];
+    $c_pin=$_POST['c-pin'];
+    $new_phone=$_POST['phon_eno'];
+    if($new_phone==$phone){
+        if($old_pin==$account_no){
+        if($new_pin==$c_pin){
+            $updatequerry="update information set account_no = '$new_pin' where account_no='$account_no'";
+            $updatesql=mysqli_query($con,$updatequerry);
+            header("location:logout.php");
+        }else{
+            echo "new pin mismatch";
+        }
+    }else{
+        echo "old pin mismatch";
+    }
+    }
+    else {
+        echo "phone mismatch";
+    }
+}
 ?>
